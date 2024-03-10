d01c9f89737b7bf74f2b0be462bcf7eeb000b719
Cagkan <99582229+cyldztkn@users.noreply.github.com>
2024-03-10 19:48:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import Icon from "@mdi/react";
+import { mdiAccount,mdiCollage } from "@mdi/js";
+
+function App() {
+  return (
+    <>
+      <section>
+    
+      </section>
+    </>
+  );
+}
+
+export default App;
