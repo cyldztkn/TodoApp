bf208193215ce99b64d81b3f0a7382b7236642b2
Cagkan <99582229+cyldztkn@users.noreply.github.com>
2024-03-17 14:09:22 +0300

    build deneme

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
+
+  base: '/TodoApp/', // Bu kısmı projenizin GitHub Pages URL'sine göre ayarlayın
+
 })
